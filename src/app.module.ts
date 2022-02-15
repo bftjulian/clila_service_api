b1b534ee5b2d6ee52fcbdbadca241c32d19820ab
Gabriel Padilha <gabriel.padilha@brasilfone.com.br>
2022-02-15 14:26:20 -0300

    [DVB-2996] - Created Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { LinksModule } from './links/links.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, LinksModule],
   controllers: [AppController],
   providers: [AppService],
 })
