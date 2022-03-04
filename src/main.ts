65621a76fd32fba4ec4c6a474dbe11c944a2ebd5
Nicacio Brasilfone <nicacio@brasilfone.com.br>
2022-03-04 09:34:35 -0300

    DVB-3293 disabled cors in clila

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: '*',
+    exposedHeaders: ['x-total-count'],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
