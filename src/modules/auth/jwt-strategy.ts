053a22ecf9e4cb41fe2c3020f47460bef755d91e
Gabriel Padilha <gabriel.padilha@brasilfone.com.br>
2022-03-22 10:27:56 -0300

    [DVB-3511] - Changed Token Expiration logic

--- src/modules/auth/jwt-strategy.ts
+++ src/modules/auth/jwt-strategy.ts
@@ -1,17 +1,31 @@
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy, VerifiedCallback } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { isAfter } from 'date-fns';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { jwtConstants } from './constants';
 import { IUserTokenDto } from './dtos/user-token.dto';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: jwtConstants.secret,
-    });
+    super(
+      {
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        ignoreExpiration: true,
+        secretOrKey: jwtConstants.secret,
+      },
+      (payload: any, done: VerifiedCallback) => {
+        const expiresAt = new Date(payload.exp * 1000);
+        if (isAfter(Date.now(), expiresAt)) {
+          throw new UnauthorizedException({
+            statusCode: 401,
+            message: 'token.expired',
+          });
+        }
+
+        return done(null, payload);
+      },
+    );
   }
 
   async validate(payload: any): Promise<IUserTokenDto> {
