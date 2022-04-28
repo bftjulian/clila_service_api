e1fea5ed1631b3963db99467850b03d8d17de806
Nicacio Brasilfone <nicacio@brasilfone.com.br>
2022-04-28 10:51:18 -0300

    DVB-4014 adding permanent link column

--- src/modules/links/schemas/link.schema.ts
+++ src/modules/links/schemas/link.schema.ts
@@ -1,54 +1,67 @@
 import * as mongoose from 'mongoose';
 
-export const LinkSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    default: null,
-  },
-  surname: {
-    type: String,
-    trim: true,
-    default: null,
-  },
-  original_link: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  short_link: {
-    type: String,
-    required: true,
-    trim: true,
-    index: {
-      unique: true,
+export const LinkSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      default: null,
+    },
+    surname: {
+      type: String,
+      trim: true,
+      default: null,
+    },
+    original_link: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    short_link: {
+      type: String,
+      required: true,
+      trim: true,
+      index: {
+        unique: true,
+      },
+    },
+    hash_link: {
+      type: String,
+      required: true,
+      trim: true,
+      index: {
+        unique: true,
+      },
+    },
+    numbers_clicks: {
+      type: Number,
+      trim: true,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: false,
+    },
+    active: {
+      type: Boolean,
+      trim: true,
+      default: true,
+    },
+    permanent_link: {
+      type: Boolean,
+      trim: true,
+      default: false,
+    },
+    create_at: {
+      type: Date,
+      trim: true,
+    },
+    update_at: {
+      type: Date,
+      trim: true,
+      default: null,
     },
   },
-  hash_link: {
-    type: String,
-    required: true,
-    trim: true,
-    index: {
-      unique: true,
-    },
-  },
-  numbers_clicks: {
-    type: Number,
-    trim: true,
-    default: 0,
-  },
-  create_at: {
-    type: Date,
-    trim: true,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: false,
-  },
-  active: {
-    type: Boolean,
-    trim: true,
-    default: true,
-  },
-});
+  { timestamps: true },
+);
