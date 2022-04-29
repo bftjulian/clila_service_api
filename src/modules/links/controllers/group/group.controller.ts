f73e5c5bc376e5ea7b2d003d0819ef2d83e20e57
Nicacio Brasilfone <nicacio@brasilfone.com.br>
2022-04-29 15:43:59 -0300

    DVB-3980 fix

--- src/modules/links/controllers/group/group.controller.ts
+++ src/modules/links/controllers/group/group.controller.ts
@@ -3,12 +3,8 @@ import { SkipThrottle } from '@nestjs/throttler';
 import { I18nLang } from 'nestjs-i18n';
 import { IUserTokenDto } from 'src/modules/auth/dtos/user-token.dto';
 import { JwtAuthGuard } from 'src/modules/auth/jwt-auth.guard';
-<<<<<<< HEAD:src/modules/group/controllers/group/group.controller.ts
-import { GroupService } from '../../services/group/group.service';
-=======
-import { CreateGroupDto } from '../../dtos/create-group.dto';
+// import { CreateGroupDto } from '../../dtos/create-group.dto';
 import { GroupService } from '../../service/group/group.service';
->>>>>>> b81df136a5bb05ba4e33462347fe06e489d699a2:src/modules/links/controllers/group/group.controller.ts
 
 @Controller('api/groups')
 export class GroupController {
