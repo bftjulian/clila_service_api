9dbeba2e0df4ea614936574e07c037da2b062a90
bftjulian <julian@brasilfone.com.br>
2022-07-11 15:17:31 -0300

    DVB-4893 Created dashboard basic services

--- src/modules/dashboard/dashboard.module.ts
+++ src/modules/dashboard/dashboard.module.ts
@@ -1,24 +1,36 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { LinkRepository } from '../links/repositories/implementations/link.repository';
-import { LinkInfosSchema } from '../links/schemas/link-infos.schema';
-import { LinkSchema } from '../links/schemas/link.schema';
-import { UserRepository } from '../users/repositories/implementation/user.repository';
-import { RefreshTokenSchema } from '../users/schemas/refresh-tokens.schema';
 import { UserSchema } from '../users/schemas/user.schema';
-import { DashboardController } from './controllers/dashboard/dashboard.controller';
+import { LinkSchema } from '../links/schemas/link.schema';
+import { GroupSchema } from '../links/schemas/groups.schema';
+import { DashboardGateway } from './gateways/dashboard.gateway';
+import { LinkInfosSchema } from '../links/schemas/link-infos.schema';
 import { DashboardService } from './services/dashboard/dashboard.service';
+import { RefreshTokenSchema } from '../users/schemas/refresh-tokens.schema';
+import { DashboardController } from './controllers/dashboard/dashboard.controller';
+import { UserRepository } from '../users/repositories/implementation/user.repository';
+import { LinkRepository } from '../links/repositories/implementations/link.repository';
+import { DashboardRepository } from './repositories/implementations/dashboard.repository';
+import { CacheDataRepository } from './repositories/implementations/cache-data.repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: 'Link', schema: LinkSchema },
-      { name: 'LinkInfos', schema: LinkInfosSchema },
       { name: 'User', schema: UserSchema },
+      { name: 'Link', schema: LinkSchema },
+      { name: 'Group', schema: GroupSchema },
+      { name: 'LinkInfos', schema: LinkInfosSchema },
       { name: 'RefreshToken', schema: RefreshTokenSchema },
     ]),
   ],
   controllers: [DashboardController],
-  providers: [DashboardService, LinkRepository, UserRepository],
+  providers: [
+    UserRepository,
+    LinkRepository,
+    DashboardGateway,
+    DashboardService,
+    DashboardRepository,
+    CacheDataRepository,
+  ],
 })
 export class DashboardModule {}
--- src/modules/dashboard/services/dashboard/dashboard.service.ts
+++ src/modules/dashboard/services/dashboard/dashboard.service.ts
@@ -1,20 +1,158 @@
-import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { I18nService } from 'nestjs-i18n';
-import { IUserTokenDto } from '../../../../modules/auth/dtos/user-token.dto';
-import { LinkRepository } from '../../../../modules/links/repositories/implementations/link.repository';
-import { ILinkRepository } from '../../../../modules/links/repositories/link-repository.interface';
-import { UserRepository } from '../../../../modules/users/repositories/implementation/user.repository';
-import { IUserRepository } from '../../../../modules/users/repositories/user-repository.interface';
 import { Result } from '../../../../shared/models/result';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { IUserTokenDto } from '../../../../modules/auth/dtos/user-token.dto';
+import { DashboardRepository } from '../../repositories/implementations/dashboard.repository';
+import { CacheDataRepository } from '../../repositories/implementations/cache-data.repository';
+import { ILinkRepository } from '../../../../modules/links/repositories/link-repository.interface';
+import { IUserRepository } from '../../../../modules/users/repositories/user-repository.interface';
+import { IDashboardRepository } from '../../repositories/interfaces/dashboard-repository.interface';
+import { ICacheDataRepository } from '../../repositories/interfaces/cache-data-repository.interface';
+import { UserRepository } from '../../../../modules/users/repositories/implementation/user.repository';
+import { LinkRepository } from '../../../../modules/links/repositories/implementations/link.repository';
 
 @Injectable()
 export class DashboardService {
   constructor(
     @Inject(LinkRepository)
     private readonly linksRepository: ILinkRepository,
+
     @Inject(UserRepository)
     private readonly usersRepository: IUserRepository,
+
+    @Inject(DashboardRepository)
+    private readonly dashboardRepository: IDashboardRepository,
+
+    @Inject(CacheDataRepository)
+    private readonly cacheDataRepository: ICacheDataRepository,
   ) {}
+
+  public async handleConnection(user: IUserTokenDto) {
+    await this.loadAllDataToCache(user);
+
+    await this.readAllDataFromCache(user);
+  }
+
+  public async loadAllDataToCache(user: IUserTokenDto) {
+    await this.loadTotalValuesToCache(user);
+
+    await this.loadClicksToCache(user);
+
+    await this.loadLinksToCache(user);
+
+    await this.loadGroupsToCache(user);
+  }
+
+  public async readAllDataFromCache(user: IUserTokenDto) {
+    const userId = user.id;
+
+    const idPattern = `dashboard:*:${userId}:*`;
+
+    const dataCached = await this.cacheDataRepository.readManyByPattern(
+      idPattern,
+    );
+
+    return dataCached;
+  }
+
+  public async loadTotalValuesToCache(user: IUserTokenDto) {
+    const userId = user.id;
+
+    const readTotalClicks = await this.dashboardRepository.readTotalClicks(
+      userId,
+    );
+    const cacheTotalClicksId = `dashboard:clicks:${userId}:total`;
+    await this.cacheDataRepository.create(cacheTotalClicksId, readTotalClicks);
+
+    const readTotalLinks = await this.dashboardRepository.readTotalLinks(
+      userId,
+    );
+    const cacheTotalLinksId = `dashboard:links:${userId}:total`;
+    await this.cacheDataRepository.create(cacheTotalLinksId, readTotalLinks);
+
+    const readTotalGroups = await this.dashboardRepository.readTotalGroups(
+      userId,
+    );
+    const cacheTotalGroupsId = `dashboard:groups:${userId}:total`;
+    await this.cacheDataRepository.create(cacheTotalGroupsId, readTotalGroups);
+  }
+
+  public async loadTotalValuesPerPeriodToCache(user: IUserTokenDto) {
+    const userId = user.id;
+
+    const readTotalClicksPerPeriod =
+      await this.dashboardRepository.readTotalClicksPerPeriod(userId);
+    readTotalClicksPerPeriod.map(async (click) => {
+      const cacheId = `dashboard:clicks:${userId}:${click.date}`;
+      await this.cacheDataRepository.create(cacheId, click);
+    });
+
+    const readTotalLinksPerPeriod =
+      await this.dashboardRepository.readTotalLinksPerPeriod(userId);
+    readTotalLinksPerPeriod.map(async (link) => {
+      const cacheId = `dashboard:links:${userId}:${link.date}`;
+      await this.cacheDataRepository.create(cacheId, link);
+    });
+
+    const readTotalGroupsPerPeriod =
+      await this.dashboardRepository.readTotalGroupsPerPeriod(userId);
+    readTotalGroupsPerPeriod.map(async (group) => {
+      const cacheId = `dashboard:groups:${userId}:${group.date}`;
+
+      await this.cacheDataRepository.create(cacheId, group);
+    });
+  }
+
+  public async loadClicksToCache(user: IUserTokenDto) {
+    const userId = user.id;
+
+    const clicksData = await this.dashboardRepository.readDataClicks(userId);
+
+    clicksData.map(async (document) => {
+      const cacheId = `dashboard:clicks:${userId}:${document.link_id}:${document.date}`;
+
+      await this.cacheDataRepository.create(cacheId, document);
+    });
+
+    return clicksData;
+  }
+
+  public async loadLinksToCache(user: IUserTokenDto) {
+    const userId = user.id;
+
+    const linksData = await this.dashboardRepository.readDataLinks(userId);
+
+    linksData.map(async (document) => {
+      let group;
+
+      if (!document.group) {
+        group = '-';
+      } else {
+        group = document.group;
+      }
+
+      const cacheId = `dashboard:links:${userId}:${group}:${document.date}`;
+
+      await this.cacheDataRepository.create(cacheId, document);
+    });
+
+    return linksData;
+  }
+
+  public async loadGroupsToCache(user: IUserTokenDto) {
+    const userId = user.id;
+
+    const groupsData = await this.dashboardRepository.readDataGroups(userId);
+
+    groupsData.map(async (document) => {
+      const cacheId = `dashboard:groups:${userId}:${document.date}`;
+
+      await this.cacheDataRepository.create(cacheId, document);
+    });
+
+    return groupsData;
+  }
+
   public async dashboard(user: IUserTokenDto) {
     try {
       const userModel = await this.usersRepository.findById(user.id);
